99021d3f64277b6692179077fc8b94b9184d394f
David Astor <toodimes@gmail.com>
2021-06-07 11:14:58 -0400

    removes HTTPotion and puts in HTTPoison

--- lib/zen_ex/http_client.ex
+++ lib/zen_ex/http_client.ex
@@ -7,7 +7,7 @@ defmodule ZenEx.HTTPClient do
   alias ZenEx.Collection
 
   def get("https://" <> _ = url) do
-    url |> HTTPotion.get([basic_auth: basic_auth()])
+    url |> HTTPoison.get(basic_auth_header())
   end
   def get(endpoint) do
     endpoint |> build_url |> get
@@ -21,19 +21,19 @@ defmodule ZenEx.HTTPClient do
 
   def post(endpoint, %{} = param, decode_as) do
     build_url(endpoint)
-    |> HTTPotion.post([body: Poison.encode!(param), headers: ["Content-Type": @content_type], basic_auth: basic_auth()])
+    |> HTTPoison.post(Poison.encode!(param), ["Content-Type": @content_type] ++ basic_auth_header())
     |> _build_entity(decode_as)
   end
 
   def put(endpoint, %{} = param, decode_as) do
     build_url(endpoint)
-    |> HTTPotion.put([body: Poison.encode!(param), headers: ["Content-Type": @content_type], basic_auth: basic_auth()])
+    |> HTTPoison.put(Poison.encode!(param), ["Content-Type": @content_type] ++ basic_auth_header())
     |> _build_entity(decode_as)
   end
 
   def delete(endpoint, decode_as), do: delete(endpoint) |> _build_entity(decode_as)
   def delete(endpoint) do
-    build_url(endpoint) |> HTTPotion.delete([basic_auth: basic_auth()])
+    build_url(endpoint) |> HTTPoison.delete(basic_auth_header())
   end
 
   def build_url(endpoint) do
@@ -41,10 +41,17 @@ defmodule ZenEx.HTTPClient do
   end
 
   def basic_auth do
-    {"#{get_env(:user)}/token", "#{get_env(:api_token)}"}
+    "#{get_env(:user)}/token:#{get_env(:api_token)}"
+    |> Base.encode64()
   end
 
-  def _build_entity(%HTTPotion.Response{} = res, [{key, [module]}]) do
+  def basic_auth_header do
+    [
+      "Authorization": "Basic #{basic_auth()}"
+    ]
+  end
+
+  def _build_entity({:ok ,%HTTPoison.Response{} = res}, [{key, [module]}]) do
     {entities, page} =
       res.body
       |> Poison.decode!(keys: :atoms, as: %{key => [struct(module)]})
@@ -52,10 +59,10 @@ defmodule ZenEx.HTTPClient do
 
     struct(Collection, Map.merge(page, %{entities: entities, decode_as: [{key, [module]}]}))
   end
-  def _build_entity(%HTTPotion.Response{} = res, [{key, module}]) do
+  def _build_entity({:ok, %HTTPoison.Response{} = res}, [{key, module}]) do
     res.body |> Poison.decode!(keys: :atoms, as: %{key => struct(module)}) |> Map.get(key)
   end
-  def _build_entity(%HTTPotion.ErrorResponse{message: error}, _) do
+  def _build_entity({error, %HTTPoison.Error{reason: error}}, _) do
     {:error, error}
   end
 
--- spec/zen_ex/http_client_spec.exs
+++ spec/zen_ex/http_client_spec.exs
@@ -11,42 +11,42 @@ defmodule ZenEx.HTTPClientSpec do
   let :headers, do: ["Content-Type": "application/json"]
   let :basic_auth, do: HTTPClient.basic_auth
   let :json_user, do: ~s({"user":{"id":223443,"name":"Johnny Agent"}})
-  let :response, do: %HTTPotion.Response{body: json_user()}
+  let :response, do: {:ok, %HTTPoison.Response{body: json_user()}}
   let :decode_as, do: [user: User]
 
   describe "get" do
-    before do: allow HTTPotion |> to(accept :get, fn(_, _) -> response() end)
+    before do: allow HTTPoison |> to(accept :get, fn(_, _) -> response() end)
 
-    it "calls HTTPotion.get" do
+    it "calls HTTPoison.get" do
       HTTPClient.get endpoint(), decode_as()
-      expect HTTPotion |> to(accepted :get, [url(), [basic_auth: basic_auth()]])
+      expect HTTPoison |> to(accepted :get, [url(), [basic_auth: basic_auth()]])
     end
   end
 
   describe "post" do
-    before do: allow HTTPotion |> to(accept :post, fn(_, _) -> response() end)
+    before do: allow HTTPoison |> to(accept :post, fn(_, _) -> response() end)
 
-    it "calls HTTPotion.post" do
+    it "calls HTTPoison.post" do
       HTTPClient.post endpoint(), param(), decode_as()
-      expect HTTPotion |> to(accepted :post, [url(), [body: body(), headers: headers(), basic_auth: basic_auth()]])
+      expect HTTPoison |> to(accepted :post, [url(), [body: body(), headers: headers(), basic_auth: basic_auth()]])
     end
   end
 
   describe "put" do
-    before do: allow HTTPotion |> to(accept :put, fn(_, _) -> response() end)
+    before do: allow HTTPoison |> to(accept :put, fn(_, _) -> response() end)
 
-    it "calls HTTPotion.put" do
+    it "calls HTTPoison.put" do
       HTTPClient.put endpoint(), param(), decode_as()
-      expect HTTPotion |> to(accepted :put, [url(), [body: body(), headers: headers(), basic_auth: basic_auth()]])
+      expect HTTPoison |> to(accepted :put, [url(), [body: body(), headers: headers(), basic_auth: basic_auth()]])
     end
   end
 
   describe "delete" do
-    before do: allow HTTPotion |> to(accept :delete, fn(_, _) -> response() end)
+    before do: allow HTTPoison |> to(accept :delete, fn(_, _) -> response() end)
 
-    it "calls HTTPotion.delete" do
+    it "calls HTTPoison.delete" do
       HTTPClient.delete endpoint()
-      expect HTTPotion |> to(accepted :delete, [url(), [basic_auth: basic_auth()]])
+      expect HTTPoison |> to(accepted :delete, [url(), [basic_auth: basic_auth()]])
     end
   end
 
@@ -72,7 +72,7 @@ defmodule ZenEx.HTTPClientSpec do
       let :json_users do
         ~s({"count":2,"users":[{"id":223443,"name":"Johnny Agent"},{"id":8678530,"name":"James A. Rosen"}]})
       end
-      let :response, do: %HTTPotion.Response{body: json_users()}
+      let :response, do: %HTTPoison.Response{body: json_users()}
       let :decode_as, do: [users: [User]]
 
       it "returns %Collection{}" do
